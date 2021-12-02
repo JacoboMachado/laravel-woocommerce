c9f499fe55a67280bcc63b5ab45ee201be78e0ff
JacoboMachado <52087009+JacoboMachado@users.noreply.github.com>
2021-12-02 12:58:56 -0300

    Update QueryBuilderTrait.php

--- src/Traits/QueryBuilderTrait.php
+++ src/Traits/QueryBuilderTrait.php
@@ -327,7 +327,7 @@ trait QueryBuilderTrait
                 $this->options[$option] = $value;
             }
 
-            $data = $this->get();
+            $data = $this->get($sitio);
             $totalResults = WooCommerce::countResults($site);
             $totalPages = WooCommerce::countPages($site);
             $currentPage = WooCommerce::current($site);
